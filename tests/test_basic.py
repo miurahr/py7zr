c781759fd67fb224635d3f2a8c77770999e81f90
Hiroshi Miura <miurahr@linux.com>
2019-03-17 21:09:57 +0900

    Update testcase
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,13 +1,8 @@
-import pytest
+import os
 import py7zr
+import pytest
+
 
 def test_extract_all():
-    archive = py7zr.Archive7z(open('tests/test.7z', 'rb'))
-    for name in archive.getnames():
-        outfilename = os.path.join('/tmp/py7zrtest', name)
-        outdir = os.path.dirname(outfilename)
-        if not os.path.exists(outdir):
-            os.makedirs(outdir)
-        outfile = open(outfilename, 'wb')
-        outfile.write(archive.getmember(name).read())
-        outfile.close()
+    archive = py7zr.Archive(open('tests/test.7z', 'rb'))
+    archive.extract_all(dest='/tmp/py7zr-test')
