4176030f476b0cafde5cb1c80e002f4a55326e63
Hiroshi Miura <miurahr@linux.com>
2019-03-17 22:50:27 +0900

    test: add assertion
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,27 @@
-import os
 import py7zr
-import pytest
+from io import StringIO
 
 
-def test_extract_all():
+def test_list_1():
+    archive = py7zr.Archive(open('tests/test.7z', 'rb'))
+    output = StringIO()
+    archive.list(file=output)
+    contents = output.getvalue()
+    expected = """total 3 files in solid archive
+       111       441 b36aaedb scripts/py7zr
+        58       441 dcbf8d07 setup.cfg
+       559       441 80fc72be setup.py
+"""
+    assert expected == contents
+
+def test_extract_1():
     archive = py7zr.Archive(open('tests/test.7z', 'rb'))
     archive.extract_all(dest='/tmp/py7zr-test')
+    expected = """[flake8]
+max-line-length = 125
+
+[bdist_wheel]
+universal=1
+"""
+    f = open("/tmp/py7zr-test/setup.cfg", "r")
+    assert expected == f.read()
