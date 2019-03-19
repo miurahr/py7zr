63391721a7c847a91e567b59f6aaef4c9d90bd82
Hiroshi Miura <miurahr@linux.com>
2019-03-19 21:32:18 +0900

    Test: add github_14 pylzma test case
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_pylzma_cases.py
+++ tests/test_pylzma_cases.py
@@ -47,3 +47,34 @@ def test_empty():
     # decompress empty archive
     archive = py7zr.Archive(open('tests/archive/empty.7z', 'rb'))
     assert archive.getnames() == []
+
+def test_github_14():
+    archive = py7zr.Archive(open('tests/archive/github_14.7z', 'rb'))
+    assert archive.getnames() == ['github_14']
+    cf = archive.getmember('github_14')
+    assert cf != None
+    data = cf.read()
+    assert len(data) == cf.uncompressed
+    assert data == bytes('Hello GitHub issue #14.\n', 'ascii')
+    # accessing by name returns an arbitrary compressed streams
+    # if both don't have a name in the archive
+    archive = py7zr.Archive(open('tests/archive/github_14_multi.7z', 'rb'))
+    assert archive.getnames() == ['github_14_multi', 'github_14_multi']
+    cf = archive.getmember('github_14_multi')
+    assert cf != None
+    data = cf.read()
+    assert len(data) == cf.uncompressed
+    assert (data in (bytes('Hello GitHub issue #14 1/2.\n', 'ascii'), bytes('Hello GitHub issue #14 2/2.\n', 'ascii'))) == True
+    # accessing by index returns both values
+    cf = archive.getmember(0)
+    assert cf != None
+    data = cf.read()
+    assert len(data) == cf.uncompressed
+    assert data == bytes('Hello GitHub issue #14 1/2.\n', 'ascii')
+    cf = archive.getmember(1)
+    assert cf != None
+    data = cf.read()
+    assert len(data) == cf.uncompressed
+    assert data == bytes('Hello GitHub issue #14 2/2.\n', 'ascii')
+
+
