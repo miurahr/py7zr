b5c24e0204fd015338fc024f6a1a55e095206c5e
Hiroshi Miura <miurahr@linux.com>
2019-06-19 22:16:26 +0900

    PEP8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ package_name = "py7zr"
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
 
+
 def readme():
     with io.open(os.path.join(os.path.dirname(__file__), 'README.rst'), mode="r", encoding="UTF-8") as readmef:
         return readmef.read()
+
 
 with open(os.path.join(root_dir, package_name, '__init__.py')) as f:
     init_text = f.read()
@@ -39,28 +41,23 @@ setup(name=package_name,
       author_email=author_email,
       packages=[package_name],
       requires=['bringbuf'],
-      extras_require={
-        'dev': [
-            'pytest'
-        ]
-      },
+      extras_require={'dev': ['pytest']},
       scripts=["bin/py7zr"],
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: System :: Archiving :: Compression',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-      ]
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Topic :: System :: Archiving :: Compression',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ]
       )
