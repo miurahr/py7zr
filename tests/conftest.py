0429ab1cdb5f9370e6db5fbba8b9e9139f2b56fe
Hiroshi Miura <miurahr@linux.com>
2019-06-26 07:35:30 +0900

    Add pyannotate
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+# Configuration for pytest to automatically collect types.
+# Thanks to Guilherme Salgado.
+import pytest
+
+
+def pytest_collection_finish(session):
+    """Handle the pytest collection finish hook: configure pyannotate.
+    Explicitly delay importing `collect_types` until all tests have
+    been collected.  This gives gevent a chance to monkey patch the
+    world before importing pyannotate.
+    """
+    from pyannotate_runtime import collect_types
+    collect_types.init_types_collection()
+
+
+@pytest.fixture(autouse=True)
+def collect_types_fixture():
+    from pyannotate_runtime import collect_types
+    collect_types.start()
+    yield
+    collect_types.stop()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    from pyannotate_runtime import collect_types
+    collect_types.dump_stats("type_info.json")
