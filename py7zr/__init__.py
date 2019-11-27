1f5e42b1ab7627d12f7b443c9167c6298b35bcfb
Hiroshi Miura <miurahr@linux.com>
2019-11-27 12:53:57 +0900

    Introduce setuptools-scm
    - Version automation with setuptools-scm
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- py7zr/__init__.py
+++ py7zr/__init__.py
@@ -17,17 +17,24 @@
 #    License along with this library; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+from pkg_resources import DistributionNotFound, get_distribution
+
 from py7zr.exceptions import (Bad7zFile, DecompressionError,
                               UnsupportedCompressionMethodError)
 from py7zr.py7zr import (ArchiveInfo, FileInfo, SevenZipFile, is_7zfile,
                          unpack_7zarchive)
 
 __copyright__ = 'Copyright (C) 2019 Hiroshi Miura'
-__version__ = '0.4b1'
 __license__ = 'LGPL-2.1+'
 __author__ = 'Hiroshi Miura'
 __author_email__ = 'miurahr@linux.com'
 __url__ = 'http://github.com/miurahr/py7zr'
 
-__all__ = ['ArchiveInfo', 'FileInfo', 'SevenZipFile', 'is_7zfile',
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
+
+__all__ = ['__version__', 'ArchiveInfo', 'FileInfo', 'SevenZipFile', 'is_7zfile',
            'UnsupportedCompressionMethodError', 'Bad7zFile', 'DecompressionError', 'unpack_7zarchive']
--- setup.py
+++ setup.py
@@ -18,20 +18,18 @@ def readme():
 
 with open(os.path.join(root_dir, package_name, '__init__.py')) as f:
     init_text = f.read()
-    version = re.search(r'__version__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     license = re.search(r'__license__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     author = re.search(r'__author__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     author_email = re.search(r'__author_email__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
     url = re.search(r'__url__\s*=\s*[\'\"](.+?)[\'\"]', init_text).group(1)
 
-assert version
 assert license
 assert author
 assert author_email
 assert url
 
 setup(name=package_name,
-      version=version,
+      use_scm_version=True,
       description='Pure python 7-zip decompression(restricted) library',
       url='http://github.com/miurahr/py7zr',
       license=license,
@@ -40,7 +38,8 @@ setup(name=package_name,
       author=author,
       author_email=author_email,
       packages=[package_name],
-      requires=['texttable'],
+      install_requires=['texttable'],
+      setup_requires=['setuptools-scm>=3.3', 'setuptools>=42.0'],
       extras_require={'dev': ['pytest']},
       scripts=["bin/py7zr"],
       classifiers=['Development Status :: 4 - Beta',
