06b5ee6497ba9856d86d98512fb5befecae3c706
Hiroshi Miura <miurahr@linux.com>
2020-02-13 16:15:49 +0900

    PEP8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -255,7 +255,7 @@ def test_extract_ppmd(tmp_path):
         archive.extractall(path=tmp_path)
         archive.close()
 
-        
+
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="Normal user is not permitted to create symlinks.")
 def test_extract_symlink_with_relative_target_path(tmp_path):
     archive = py7zr.SevenZipFile(open(os.path.join(testdata_path, 'symlink.7z'), 'rb'))
