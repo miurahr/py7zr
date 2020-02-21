03befe24b6bc2d4cfa54575d4507bdcec0a2a832
Hiroshi Miura <miurahr@linux.com>
2020-02-21 20:13:42 +0900

    test: limit concurrency
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_concurrent.py
+++ tests/test_concurrent.py
@@ -10,7 +10,7 @@ import py7zr
 
 
 def download(pool, archive, url):
-    pool.acquire(blocking=True)
+    pool.acquire()
     try:
         logging.getLogger().info("Start Downloading {}".format(url))
         resp = urlopen(url)
@@ -25,26 +25,33 @@ def download(pool, archive, url):
     pool.release()
 
 
-def extract(archive, base_dir):
+def extract(pool, archive, base_dir):
+    pool.acquire()
     logging.getLogger().info("Extracting {}".format(archive))
     py7zr.SevenZipFile(archive).extractall(path=base_dir)
+    pool.release()
 
 
-@pytest.mark.timeout(120)
+@pytest.mark.timeout(600)
 @pytest.mark.remote_data
 def test_concurrent_run(tmp_path, caplog):
     archives = [(tmp_path.joinpath('qt3d.7z'),
-                 'https://ftp.jaist.ac.jp/pub/qtproject/online/qtsdkrepository/windows_x86/desktop/'
-                 'qt5_5126/qt.qt5.5126.win64_mingw73/'
+                 'https://ftp.jaist.ac.jp/pub/qtproject/online/qtsdkrepository/'
+                 'windows_x86/desktop/qt5_5126/qt.qt5.5126.win64_mingw73/'
                  '5.12.6-0-201911111120qt3d-Windows-Windows_10-Mingw73-Windows-Windows_10-X86_64.7z'),
-                (tmp_path.joinpath('qtbase.7z'),
-                 'http://ftp.jaist.ac.jp/pub/qtproject/online/qtsdkrepository/windows_x86/desktop/'
-                 'qt5_5132/qt.qt5.5132.win32_mingw73/'
-                 '5.13.2-0-201910281254qtbase-Windows-Windows_7-Mingw73-Windows-Windows_7-X86.7z'),
+                (tmp_path.joinpath('qtxmlpatterns.7z'),
+                 'https://ftp1.nluug.nl/languages/qt/online/qtsdkrepository/'
+                 'windows_x86/desktop/qt5_5132/qt.qt5.5132.win64_mingw73/'
+                 '5.13.2-0-201910281254qtxmlpatterns-Windows-Windows_10-Mingw73-Windows-Windows_10-X86_64.7z'),
                 (tmp_path.joinpath('qtactiveqt.7z'),
-                 'http://mirrors.dotsrc.org/qtproject/online/qtsdkrepository/windows_x86/desktop/'
-                 'qt5_5132/qt.qt5.5132.win64_mingw73/'
+                 'http://mirrors.dotsrc.org/qtproject/online/qtsdkrepository/'
+                 'windows_x86/desktop/qt5_5132/qt.qt5.5132.win64_mingw73/'
                  '5.13.2-0-201910281254qtactiveqt-Windows-Windows_10-Mingw73-Windows-Windows_10-X86_64.7z'),
+                (tmp_path.joinpath('qtbase.7z'),
+                 # 'http://qt.mirrors.tds.net/qt/online/qtsdkrepository/'
+                 'https://ftp.jaist.ac.jp/pub/qtproject/online/qtsdkrepository/'
+                 'windows_x86/desktop/qt5_5132/qt.qt5.5132.win32_mingw73/'
+                 '5.13.2-0-201910281254qtbase-Windows-Windows_7-Mingw73-Windows-Windows_7-X86.7z'),
                 (tmp_path.joinpath('opengl32sw.7z'),
                  'http://mirrors.ocf.berkeley.edu/qt/online/qtsdkrepository/windows_x86/desktop/'
                  'qt5_5132/qt.qt5.5132.win64_mingw73/'
@@ -58,7 +65,8 @@ def test_concurrent_run(tmp_path, caplog):
     caplog.set_level(logging.INFO)
     start_time = time.perf_counter()
     # Limit the number of threads of download.
-    pool = threading.BoundedSemaphore(6)
+    pool = threading.BoundedSemaphore(3)
+    ex_pool = multiprocessing.BoundedSemaphore(6)
     download_threads = []
     extract_processes = []
     completed_downloads = []
@@ -75,7 +83,7 @@ def test_concurrent_run(tmp_path, caplog):
                 t.join(0.05)
                 if not t.is_alive():
                     completed_downloads[i] = True
-                    p = ctx.Process(target=extract, args=(a, tmp_path))
+                    p = ctx.Process(target=extract, args=(ex_pool, a, tmp_path))
                     extract_processes.append(p)
                     p.start()
                 else:
