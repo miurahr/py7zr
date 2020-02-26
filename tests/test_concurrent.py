cdd4d18cedeb114847be4c0e0a0f8fdfbf4e4cf5
Hiroshi Miura <miurahr@linux.com>
2020-02-26 19:47:42 +0900

    simplify concurrent tests
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_concurrent.py
+++ tests/test_concurrent.py
@@ -1,11 +1,8 @@
 import concurrent.futures
-import functools
 import logging
 import ssl
 import sys
-import threading
 import time
-from operator import and_
 from urllib.request import urlopen
 
 import pytest
@@ -14,187 +11,6 @@ import py7zr
 
 # hack only for the test, it is highly discouraged for production.
 ssl._create_default_https_context = ssl._create_unverified_context
-
-
-class MyThreadRun:
-    def __init__(self):
-        pass
-
-    def download(self, pool, archive, url, results):
-        pool.acquire()
-        try:
-            logging.getLogger().info("Start Downloading {}".format(url))
-            resp = urlopen(url)
-            with archive.open('wb') as fd:
-                while True:
-                    chunk = resp.read(8196)
-                    if not chunk:
-                        break
-                    fd.write(chunk)
-        except Exception:
-            results[archive] = False
-        else:
-            results[archive] = True
-        pool.release()
-
-    def extract(self, pool, archive, base_dir, results):
-        pool.acquire()
-        try:
-            logging.getLogger().info("Extracting {}".format(archive))
-            szf = py7zr.SevenZipFile(archive)
-            szf.extractall(path=base_dir)
-            szf.close()
-        except Exception:
-            results[archive] = False
-        else:
-            results[archive] = True
-        pool.release()
-
-    def download_and_extract(self, archives, tmp_path):
-        # Limit a number of threads of download.
-        pool = threading.BoundedSemaphore(3)
-        # Limit a number of extraction threads.
-        ex_pool = threading.BoundedSemaphore(6)
-        download_threads = {}
-        download_results = {}
-        extract_threads = {}
-        completed_downloads = {}
-        completed_extracts = {}
-        extract_results = {}
-        for ar in archives:
-            fname = tmp_path.joinpath(ar[0])
-            url = ar[1]
-            t = threading.Thread(target=self.download, args=(pool, fname, url, download_results))
-            download_threads[fname] = t
-            extract_threads[fname] = None
-            completed_downloads[fname] = False
-            completed_extracts[fname] = False
-            t.start()
-        while True:
-            all_done = True
-            for a in download_threads:
-                if not completed_downloads[a]:
-                    t = download_threads[a]
-                    t.join(0.05)
-                    if not t.is_alive():
-                        completed_downloads[a] = True
-                        ex = threading.Thread(target=self.extract, args=(ex_pool, a, tmp_path, extract_results))
-                        extract_threads[a] = ex
-                        ex.start()
-                    else:
-                        all_done = False
-                elif completed_extracts[a] or extract_threads[a] is None:
-                    pass
-                else:
-                    ex = extract_threads[a]
-                    ex.join(0.005)
-                    if not ex.is_alive():
-                        if extract_results[a]:
-                            completed_extracts[a] = True
-                        else:
-                            raise Exception("Extraction error.")
-            if all_done:
-                break
-            time.sleep(0.5)
-        for a in extract_threads:
-            if not completed_extracts[a]:
-                ex = extract_threads[a]
-                ex.join()
-                if not extract_results[a]:
-                    raise Exception("Extraction error.")
-                else:
-                    completed_extracts[a] = True
-
-
-class FuturesRun:
-    def __init__(self):
-        pass
-
-    def download(self, ar, path):
-        archive = path.joinpath(ar[0])
-        url = ar[1]
-        try:
-            resp = urlopen(url)
-            with open(archive, 'wb') as fd:
-                while True:
-                    chunk = resp.read(8196)
-                    if not chunk:
-                        break
-                    fd.write(chunk)
-        except Exception:
-            exc = sys.exc_info()
-            logging.error("Caught download error: %s" % exc[1])
-            return False, None
-        return True, time.process_time()
-
-    def extract(self, ar, path):
-        archive = path.joinpath(ar[0])
-        try:
-            szf = py7zr.SevenZipFile(archive)
-            szf.extractall(path=path)
-            szf.close()
-        except Exception:
-            exc = sys.exc_info()
-            logging.error("Caught extraction error: %s" % exc[1])
-            return False, None
-        return True, time.process_time()
-
-    def download_and_extract(self, archives, tmp_path):
-        download_task = []
-        extract_task = []
-        completed_downloads = []
-        completed_extracts = []
-        with concurrent.futures.ThreadPoolExecutor() as texec:
-            for i, ar in enumerate(archives):
-                url = ar[1]
-                logging.info("Downloading {}...".format(url))
-                download_task.append(texec.submit(self.download, ar, tmp_path))
-                extract_task.append(None)
-                completed_downloads.append(False)
-                completed_extracts.append(False)
-            while True:
-                for i, ar in enumerate(archives):
-                    if not completed_downloads[i]:
-                        # check download status
-                        task = download_task[i]
-                        if task.done():
-                            (res, elapsed) = task.result()
-                            if not res:
-                                raise Exception("Failed to download {}".format(ar[0]))
-                            completed_downloads[i] = True
-                            # start extraction
-                            logging.info("Extracting {}...".format(ar[0]))
-                            extract_task[i] = texec.submit(self.extract, ar, tmp_path)
-                    time.sleep(0.005)
-                if functools.reduce(and_, completed_downloads):
-                    logging.info("Downloads are Completed.")
-                    break
-                else:
-                    for j, arc in enumerate(archives):
-                        task = extract_task[j]
-                        if task is None:
-                            continue
-                        if task.done():
-                            (res, elapsed) = task.result()
-                            if not res:
-                                raise Exception("Failed to extract {}".format(arc[0]))
-                            logging.info("Done {} extraction in {:.8f}.".format(arc[0], elapsed))
-                            completed_extracts[j] = True
-                time.sleep(0.05)
-            while True:
-                for i, ar in enumerate(archives):
-                    if not completed_extracts[i]:
-                        task = extract_task[i]
-                        if task is None:
-                            raise Exception("Unconsidered status")
-                        if task.done():
-                            (res, elapsed) = task.result()
-                            logging.info("Done {} extraction in {:.8f}.".format(ar[0], elapsed))
-                            completed_extracts[i] = True
-                if functools.reduce(and_, completed_extracts):
-                    break
-                else:
-                    time.sleep(0.5)
 
 
 archives = [('qt3d.7z',
@@ -227,19 +43,64 @@ archives = [('qt3d.7z',
 
 @pytest.mark.timeout(180)
 @pytest.mark.remote_data
-def test_concurrent_run(tmp_path, caplog):
+def test_concurrent_futures(tmp_path, caplog):
+
+    def download_and_extract(self, ar, path):
+        archive = path.joinpath(ar[0])
+        url = ar[1]
+        try:
+            resp = urlopen(url)
+            with open(archive, 'wb') as fd:
+                while True:
+                    chunk = resp.read(8196)
+                    if not chunk:
+                        break
+                    fd.write(chunk)
+        except Exception:
+            exc = sys.exc_info()
+            logging.error("Caught download error: %s" % exc[1])
+            return False, None
+        archive = path.joinpath(ar[0])
+        try:
+            szf = py7zr.SevenZipFile(archive)
+            szf.extractall(path=path)
+            szf.close()
+        except Exception:
+            exc = sys.exc_info()
+            logging.error("Caught extraction error: %s" % exc[1])
+            return False, None
+        return True, time.process_time()
+
     caplog.set_level(logging.INFO)
     start_time = time.perf_counter()
-    runner = MyThreadRun()
-    runner.download_and_extract(archives, tmp_path)
+    with concurrent.futures.ThreadPoolExecutor() as texec:
+        tasks = [texec.submit(download_and_extract, ar, tmp_path) for ar in archives]
+        for task in concurrent.futures.as_completed(tasks):
+            (res, elapsed) = task.result()
+            if not res:
+                raise Exception("Failed to extract")
     logging.getLogger().info("Elapsed time {:.8f}".format(time.perf_counter() - start_time))
 
 
 @pytest.mark.timeout(180)
-@pytest.mark.remote_data
-def test_concurrent_futures(tmp_path, caplog):
+def test_concurrent_extraction(tmp_path, caplog):
+
+    def extractor(archive, path):
+        szf = py7zr.SevenZipFile(archive, 'r')
+        szf.extractall(path=path)
+        szf.close()
+        return True
+
     caplog.set_level(logging.INFO)
     start_time = time.perf_counter()
-    runner = FuturesRun()
-    runner.download_and_extract(archives, tmp_path)
+    archives = ['bugzilla_16.7z', 'bugzilla_4.7z', 'bzip2.7z', 'bzip2_2.7z', 'copy.7z',
+                'empty.7z', 'github_14.7z', 'lzma2bcj.7z', 'mblock_1.7z', 'mblock_2.7z',
+                'mblock_3.7z', 'solid.7z', 'symlink.7z', 'test_1.7z', 'test_2.7z',
+                'test_3.7z', 'test_4.7z', 'test_5.7z', 'test_6.7z',
+                'test_folder.7z', 'umlaut-non_solid.7z', 'umlaut-solid.7z', 'zerosize.7z']
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        tasks = [executor.submit(extractor, os.path.join(testdata_path, ar), tmp_path.joinpath(ar)) for ar in archives]
+        for future in concurrent.futures.as_completed(tasks):
+            if not future.result():
+                raise Exception("Extract error.")
     logging.getLogger().info("Elapsed time {:.8f}".format(time.perf_counter() - start_time))
