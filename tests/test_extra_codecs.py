4133211739345c92130e2e0f68ecfb3af20702e5
Hiroshi Miura <miurahr@linux.com>
2020-05-13 14:14:10 +0900

    Drop lz4/brotli because of non majority
    There is only single implementation of these
    compression algorithm in 7zip.
    It is difficult to keep interoperability for that.
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_extra_codecs.py
+++ tests/test_extra_codecs.py
@@ -10,14 +10,6 @@ try:
     import zstandard as Zstd  # type: ignore  # noqa
 except ImportError:
     Zstd = None
-try:
-    import lz4.stream as LZ4  # type: ignore  # noqa
-except ImportError:
-    LZ4 = None
-try:
-    import brotli as Brotli  # type: ignore  # noqa
-except ImportError:
-    Brotli = None
 
 testdata_path = pathlib.Path(os.path.dirname(__file__)).joinpath('data')
 os.umask(0o022)
@@ -55,20 +47,4 @@ def test_extract_deflate(tmp_path):
 @pytest.mark.skipif(Zstd is None, reason="No zstandard library exist.")
 def test_extract_zstd(tmp_path):
     with py7zr.SevenZipFile(testdata_path.joinpath('zstd.7z').open(mode='rb')) as archive:
-        archive.extractall(path=tmp_path)
-
-
-@pytest.mark.files
-@pytest.mark.skipif(LZ4 is None, reason="No LZ4 library exist.")
-@pytest.mark.xfail(reason="Incomplete implementation")
-def test_extract_lz4(tmp_path):
-    with py7zr.SevenZipFile(testdata_path.joinpath('lz4.7z').open(mode='rb')) as archive:
-        archive.extractall(path=tmp_path)
-
-
-@pytest.mark.files
-@pytest.mark.skipif(Brotli is None, reason="No Brotli library exist.")
-@pytest.mark.xfail(reason="Incomplete implementation")
-def test_extract_brotli(tmp_path):
-    with py7zr.SevenZipFile(testdata_path.joinpath('brotli.7z').open(mode='rb')) as archive:
         archive.extractall(path=tmp_path)
