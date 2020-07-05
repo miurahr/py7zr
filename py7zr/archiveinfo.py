f82891b456db47438ecb265e8ab26e23efd93968
Hiroshi Miura <miurahr@linux.com>
2020-07-06 01:10:53 +0900

    isort
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- py7zr/archiveinfo.py
+++ py7zr/archiveinfo.py
@@ -21,7 +21,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-
 import functools
 import io
 import os
--- py7zr/properties.py
+++ py7zr/properties.py
@@ -20,7 +20,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-
 import binascii
 import lzma
 from enum import Enum
