ddf27ae56b4bd8c073d562fdc696967b8ae37377
Hiroshi Miura <miurahr@linux.com>
2020-07-18 17:22:20 +0900

    BCJ: performance tuning
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- py7zr/compressor.py
+++ py7zr/compressor.py
@@ -265,6 +265,9 @@ class ZstdCompressor(ISevenZipCompressor):
 
 class BCJFilter:
 
+    _mask_to_allowed_number = [0, 1, 2, 4, 8, 9, 10, 12]
+    _mask_to_bit_number = [0, 1, 2, 2, 3, 3, 3, 3]
+
     def __init__(self, is_encoder: bool):
         self.is_encoder = is_encoder  # type: bool
         #
@@ -274,8 +277,6 @@ class BCJFilter:
         self.buffer = bytearray()
 
     def x86_code(self) -> int:
-        mask_to_allowed_status = [True, True, True, False, True, False, False, False]
-        mask_to_bit_number = [0, 1, 2, 2, 3, 3, 3, 3]
         size = len(self.buffer)
         if size < 5:
             return 0
@@ -285,7 +286,7 @@ class BCJFilter:
         limit = size - 5
         buffer_pos = 0
         while buffer_pos <= limit:
-            if view[buffer_pos] != 0xE8 and view[buffer_pos] != 0xE9:
+            if view[buffer_pos] not in [0xe8, 0xe9]:
                 buffer_pos += 1
                 continue
             offset = self.current_position + buffer_pos - self.prev_pos
@@ -296,25 +297,31 @@ class BCJFilter:
                 for i in range(offset):
                     self.prev_mask &= 0x77
                     self.prev_mask <<= 1
-            if (view[buffer_pos + 4] == 0 or view[buffer_pos + 4] == 0xFF) and \
-                    mask_to_allowed_status[(self.prev_mask >> 1) & 0x7] and (self.prev_mask >> 1) < 0x10:
+            # note:
+            # condition (self.prev_mask >> 1) in [0, 1, 2, 4, 8, 9, 10, 12]
+            # is as same as
+            # condition _mask_to_allowed_status[(self.prev_mask >> 1) & 0x7] and (self.prev_mask >> 1) < 0x10:
+            # when _mask_to_allowed_status = [True, True, True, False, True, False, False, False]
+            #
+            if view[buffer_pos + 4] in [0, 0xff] and (self.prev_mask >> 1) in self._mask_to_allowed_number:
                 jump_target = self.buffer[buffer_pos + 1:buffer_pos + 5]
                 src = struct.unpack('<L', jump_target)[0]
+                distance = self.current_position + buffer_pos + 5
+                idx = self._mask_to_bit_number[self.prev_mask >> 1]
                 while True:
                     if self.is_encoder:
-                        dest = src + (self.current_position + buffer_pos + 5)
+                        dest = src + distance
                     else:
-                        dest = src - (self.current_position + buffer_pos + 5)
+                        dest = src - distance
                     if self.prev_mask == 0:
                         break
-                    i = mask_to_bit_number[self.prev_mask >> 1]
-                    b = 0xFF & (dest >> (24 - i * 8))
+                    b = 0xFF & (dest >> (24 - idx * 8))
                     if not (b == 0 or b == 0xFF):
                         break
-                    src = dest ^ (1 << (32 - i * 8) - 1)
+                    src = dest ^ (1 << (32 - idx * 8) - 1)
                 write_view = view[buffer_pos + 1:buffer_pos + 5]
                 write_view[0:3] = (dest & 0xFFFFFF).to_bytes(3, 'little')
-                write_view[3:4] = (0xFF & (~(((dest >> 24) & 1) - 1))).to_bytes(1, 'little')
+                write_view[3:4] = [b'\x00', b'\xff'][(dest >> 24) & 1]  # (~(((dest >> 24) & 1) - 1)) & 0xFF
                 buffer_pos += 5
                 self.prev_mask = 0
             else:
