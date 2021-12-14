d69a6381255b6c4266513904eb1f8b9b4c3cae05
Hiroshi Miura <miurahr@linux.com>
2021-12-15 07:38:54 +0900

    PEP8/Black
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- py7zr/compressor.py
+++ py7zr/compressor.py
@@ -772,7 +772,13 @@ class SevenZipCompressor:
         if not has_lzma2:
             # when specified other than lzma2, BCJ filters should be alternative
             for i, f in enumerate(self.filters):
-                if f["id"] == FILTER_X86 or f["id"] == FILTER_ARM or f["id"] == FILTER_ARMTHUMB or f["id"] == FILTER_SPARC or f["id"] == FILTER_POWERPC:
+                if (
+                    f["id"] == FILTER_X86
+                    or f["id"] == FILTER_ARM
+                    or f["id"] == FILTER_ARMTHUMB
+                    or f["id"] == FILTER_SPARC
+                    or f["id"] == FILTER_POWERPC
+                ):
                     self.methods_map[i] = False
         #
         if not any(self.methods_map):  # all alternative
