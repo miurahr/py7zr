579d3eced5170edff3cf1bb16fb96c2cbc5da5e1
Hiroshi Miura <miurahr@linux.com>
2022-04-18 19:54:25 +0900

    pyproject.toml: fix setuptools_scm configuration
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup.py
+++ setup.py
@@ -2,11 +2,4 @@
 
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        "local_scheme": "no-local-version",
-        "write_to": "py7zr/version.py",
-        "write_to_template": '__version__ = "{version}"\n',
-        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
-)
+setup(setup_requires=["setuptools_scm"], use_scm_version=True)
