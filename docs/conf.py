3304729e7f0a0b4c4dbc1a81c9033c2685a4c0b2
Hiroshi Miura <miurahr@linux.com>
2022-06-04 19:04:53 +0900

    docs: bump sphinx@5.0
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'py7zr'
-copyright = u'2019-2020, Hiroshi Miura'
+copyright = u'2019-2022, Hiroshi Miura'
 author = u'Hiroshi Miura'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -73,7 +73,7 @@ version = '.'.join(release.split('.')[:2])
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
