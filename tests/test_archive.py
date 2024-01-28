77bdaf5a22cac883ca394116999034bddbf048c6
diedmon <62182463+diedmon@users.noreply.github.com>
2024-01-28 20:41:57 +0800

    Update test_archive.py

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1166,8 +1166,9 @@ def test_compress_win32_absolute_arcname(tmp_path):
 
 @pytest.mark.files
 def test_empty_stream(tmp_path):
-  try:
-    with py7zr.SevenZipFile(file="test.7z",mode="w") as archive:
-      archive.writestr(data="",arcname="empty.txt")
-  except:
-    pytest.fail("Exception raised")
+    target = tmp_path.joinpath("target.7z")
+    try:
+        with py7zr.SevenZipFile(file=target,mode="w") as archive:
+        archive.writestr(data="",arcname="empty.txt")
+    except:
+        pytest.fail("Exception raised")
