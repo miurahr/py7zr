d84cf8513291a356d24284e2f82672efba062d0e
diedmon <62182463+diedmon@users.noreply.github.com>
2024-01-31 20:56:45 +0800

    Update test_archive.py

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1170,5 +1170,5 @@ def test_empty_stream(tmp_path):
     try:
         with py7zr.SevenZipFile(file="test.7z", mode="w") as archive:
         archive.writestr(data="", arcname="empty.txt")
-    except:
+    except AttributeError:
         pytest.fail("Exception raised")
