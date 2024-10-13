73c03d61e67ee1d6d7986cdd9d1da59020799cbf
Hiroshi Miura <miurahr@linux.com>
2024-10-13 10:07:31 +0900

    feat: test for backward compatibility

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,3 +1,4 @@
+import io
 import os
 import pathlib
 from typing import Optional
@@ -13,40 +14,56 @@ testdata_path = pathlib.Path(os.path.dirname(__file__)).joinpath("data")
 os.umask(0o022)
 
 
-def test_extract_stream(tmp_path, httpserver: HTTPServer):
+def test_extract_stream(httpserver: HTTPServer):
     httpserver.expect_request("/setup.cfg", method="PUT").respond_with_data("ok")
+    httpserver.expect_request("/setup.cfg", method="GET").respond_with_data("dummy data")
     httpserver.expect_request("/setup.py", method="PUT").respond_with_data("ok")
     httpserver.expect_request("/scripts/py7zr", method="PUT").respond_with_data("ok")
     factory = StreamWriterFactory(httpserver)
     with py7zr.SevenZipFile(testdata_path.joinpath("test_1.7z").open(mode="rb")) as archive:
-        archive.readall(factory=factory)
+        result = archive.readall(factory=factory)
     assert len(factory.debug_info) == 3
     assert "setup.cfg" in factory.debug_info
     assert "setup.py" in factory.debug_info
     assert "scripts/py7zr" in factory.debug_info
+    assert result["setup.cfg"].read() == b"[flake8]\nmax-line-length = 125\n\n[bdist_wheel]\nuniversal=1\n"
+
+
+def test_extract_stream_create_traditional(tmp_path, httpserver: HTTPServer):
+    httpserver.expect_request("/setup.cfg", method="PUT").respond_with_data("ok")
+    httpserver.expect_request("/setup.py", method="PUT").respond_with_data("ok")
+    httpserver.expect_request("/scripts/py7zr", method="PUT").respond_with_data("ok")
+    factory = StreamWriterFactory(httpserver)
+    with py7zr.SevenZipFile(tmp_path.joinpath("target.7z"), "w") as target:
+        with py7zr.SevenZipFile(testdata_path.joinpath("test_1.7z").open(mode="rb")) as source:
+            target.writed(source.readall(factory=factory))
 
 
 class StreamWriter(py7zr.io.Py7zIO):
     """Pseudo object storage writer."""
 
     def __init__(self, httpserver: HTTPServer, fname: str):
-        self.httpserver: HTTPServer = httpserver
+        self.buf = io.BytesIO()  # for test
+        self.httpserver: HTTPServer = httpserver  # for test
         self.fname = fname
         self.length = 0
 
     def write(self, data: [bytes, bytearray]):
         self.length += len(data)
+        self.buf.write(data)
         requests.put(self.httpserver.url_for(self.fname), data=data)
         self.httpserver.check_assertions()
 
     def read(self, size: Optional[int] = None) -> bytes:
-        return b"pseudo"
+        # This may read from other than memory.
+        # Here is a stub using BytesIO for test
+        return self.buf.read(size)
 
     def seek(self, offset: int, whence: int = 0) -> int:
-        return offset
+        return self.buf.seek(offset, whence)  # for test
 
     def flush(self) -> None:
-        pass
+        self.buf.flush()  # for test
 
     def size(self) -> int:
         return self.length
@@ -57,7 +74,7 @@ class StreamWriterFactory(py7zr.io.WriterFactory):
 
     def __init__(self, httpserver: HTTPServer):
         self.httpserver: HTTPServer = httpserver
-        self.debug_info = []
+        self.debug_info = []  # for test
 
     def create(self, filename: str) -> Py7zIO:
         self.debug_info.append(filename)
