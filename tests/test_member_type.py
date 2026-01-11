7d58d09aae7fcd99329636901dd38a8968aa8590
Hiroshi Miura <miurahr@linux.com>
2026-01-11 17:25:58 +0900

    style: isort test file
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- tests/test_member_type.py
+++ tests/test_member_type.py
@@ -5,8 +5,8 @@ from typing import NamedTuple
 import pytest
 
 from py7zr.member import (
-    FILE_ATTRIBUTE_UNIX_EXTENSION,
     FILE_ATTRIBUTE_UNIX_DEFAULT,
+    FILE_ATTRIBUTE_UNIX_EXTENSION,
     FILE_ATTRIBUTE_UNIX_SHIFT,
     FILE_ATTRIBUTE_WINDOWS_MASK,
     MemberType,
