e660526ea7406b1225a538007d360f3e7cf04e42
Hiroshi Miura <miurahr@linux.com>
2026-01-18 21:00:20 +0900

    refactor: remove unused `_lzma` import and redundant exception handling
    - Eliminated the `_lzma` import and related `LZMAError` exception handling since they are no longer used.

--- py7zr/cli.py
+++ py7zr/cli.py
@@ -31,7 +31,6 @@ import sys
 from lzma import CHECK_CRC64, CHECK_SHA256, is_check_supported
 from typing import Any
 
-import _lzma  # type: ignore
 import multivolumefile
 import texttable  # type: ignore
 
@@ -348,8 +347,6 @@ class Cli:
             else:
                 print("The archive is corrupted, or password is wrong. ABORT.")
             return 1
-        except _lzma.LZMAError:
-            return 1
 
         cb: ExtractCallback | None = None
         if verbose:
@@ -380,8 +377,6 @@ class Cli:
                 print("The archive is corrupted. ABORT.")
             else:
                 print("The archive is corrupted, or password is wrong. ABORT.")
-            return 1
-        except _lzma.LZMAError:
             return 1
         else:
             return 0
